fba3d6c216c5361cab2644b9826abdb791a433fb
Thomas Sibley <trsibley@uw.edu>
2014-01-29 16:52:34 -0800

    Initial implementation

--- lib/RTx/QuickUpdateUserComments.pm
+++ lib/RTx/QuickUpdateUserComments.pm
@@ -4,9 +4,33 @@ package RTx::QuickUpdateUserComments;
 
 our $VERSION = '0.01';
 
+RT::System->AddRight( Staff => ModifyUserComments => 'Update the Comments field on any user' );
+RT::System->AddRight( Staff => SeeUserComments    => 'Read the Comments field on any user' );
+
+package RT::User {
+    no warnings 'redefine';
+    my $original_modify = \&CurrentUserCanModify;
+    *CurrentUserCanModify = sub {
+        my ($self, $field) = @_;
+        return 1 if $field eq 'Comments'
+                and $self->CurrentUserHasRight('ModifyUserComments');
+        return $original_modify->(@_);
+    };
+
+    my $original_see = \&CurrentUserCanSee;
+    *CurrentUserCanSee = sub {
+        my ($self, $field) = @_;
+        return 1 if $field eq 'Comments'
+                and $self->CurrentUserHasRight('SeeUserComments');
+        return $original_see->(@_);
+    };
+}
+
+=encoding utf-8
+
 =head1 NAME
 
-RTx-QuickUpdateUserComments - [One line description of module's purpose here]
+RTx::QuickUpdateUserComments - Adds a quick-update comments portlet for the user summary and the rights ModifyUserComments and SeeUserComments
 
 =head1 INSTALLATION 
 
@@ -22,11 +46,14 @@ May need root permissions
 
 =item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
 
-Add this line:
+Add these lines:
 
-    Set(@Plugins, qw(RTx::QuickUpdateUserComments));
+    Plugin("RTx::QuickUpdateUserComments");
 
-or add C<RTx::QuickUpdateUserComments> to your existing C<@Plugins> line.
+    # Adds the comment edit box to the default user info portlets
+    Set( @UserSummaryPortlets, qw/ExtraInfo EditComments CreateTicket ActiveTickets InactiveTickets/ );
+
+    # Grant folks ModifyUserComments and/or SeeUserComments
 
 =item Clear your mason cache
 
@@ -46,7 +73,6 @@ All bugs should be reported via email to
 L<bug-RTx-QuickUpdateUserComments@rt.cpan.org|mailto:bug-RTx-QuickUpdateUserComments@rt.cpan.org>
 or via the web at
 L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RTx-QuickUpdateUserComments>.
-
 
 =head1 LICENSE AND COPYRIGHT
 
